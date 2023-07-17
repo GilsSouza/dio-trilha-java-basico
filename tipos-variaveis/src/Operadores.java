4f8c9a858d21b479f47d9760375381840d3787c6
origin <gilmarssouza19@gmail.com>
2023-07-17 15:42:04 -0300

    outros operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,5 +1,92 @@
 public class Operadores {
     public static void main(String[] args) {
+        // Operadores Lógicos
+
+        boolean condicao1=true;
+
+        boolean condicao2 = true;
+        
+        if (condicao1 && ( 7 > 4) ) {
+            System.out.println("as duas condições são verdadeiras");
+        }
+
+        if (condicao1 || condicao2) {
+            System.out.println("uma das condições é verdadeira");
+        }       
+
+        System.out.println("fim");
+
+//-----------------------------------------------------------------------------------------------------------------
+        /*  O peredores relacionais
+        
+        String nomeUm = "Gilmar";        
+        String nomeDois = new String("Gilmar");
+        
+        System.out.println(nomeUm.equals(nomeDois)); 
+        
+        
+        
+        int numero1 = 1;
+        int numero2 = 2;
+        
+        boolean simNao = numero1 == numero2;
+        
+        if (numero1 < numero2) {
+            System.out.println("a nossa condição é verdadeira");
+        }
+        
+        System.out.println("numeroUm é igual a numeroDois? " + simNao);
+        
+        simNao = numero1 != numero2;
+        
+        System.out.println("numeroUm é diferente a numeroDois? " + simNao);
+        
+        simNao = numero1 > numero2;
+        
+         System.out.println("numeroUm é maior que numeroDois? " + simNao);
+         
+         */
+        //------------------------------------------------------------------------
+        //operador ternario
+        /*int a, b;
+        
+        a = 6;
+        b = 6;
+        
+        //EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+        
+        /* String resultado = "";        
+        if(a==b)
+        resultado = "verdadeiro";
+        else
+        resultado = "falso";
+        
+        System.out.println(resultado); */
+
+       /*  mesmo exemplo utlizando operador ternario
+        
+       String resultado = a == b ? "verdadeiro" : "falso";
+
+       System.out.println(resultado);*/
+
+//----------------------------------------------------
+
+       /*  Boolean variavel = true;
+       
+       variavel = !variavel; // negar valor 'era verdadeiro logo se torna falso'
+       
+       System.out.println(variavel);*/
+        
+       // int numero = 5;
+
+        //x repetição
+
+        // numero = numero + 1; ou numero++;
+        
+        //System.out.println(++ numero);
+
+
+
     //    String nomeCompleto = "LINGUAGEM" + "JAVA";
         
     //    System.out.println(nomeCompleto);
@@ -24,19 +111,22 @@ public class Operadores {
     concatenacao = "1" + (1 + 1 + 1);
    
     System.out.println(concatenacao);*/
+  /*----------------------------------------------------------------- */  
     
-    
-   int numero = 5;
-
+  /*  int numero = 5;
+  
    System.out.println(numero);
-
+  
    numero = -numero;
    
    System.out.println(numero);
-
+  
    numero = numero * -1;
+  
+   System.out.println(numero);*/
 
-   System.out.println(numero);
+   
+
 
     }
 
