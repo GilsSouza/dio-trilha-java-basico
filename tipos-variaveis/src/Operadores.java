e9a929cc4db4de3597e54dc733ec3263b9362748
origin <gilmarssouza19@gmail.com>
2023-07-17 11:12:03 -0300

    Subindo operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,44 @@
+public class Operadores {
+    public static void main(String[] args) {
+    //    String nomeCompleto = "LINGUAGEM" + "JAVA";
+        
+    //    System.out.println(nomeCompleto);
+   /* String concatenacao ="?"; 
+   
+    concatenacao = 1 + 1 + 1 + "1";
+    
+    System.out.println(concatenacao);
+    
+    concatenacao = 1 + "1" + 1 + 1;
+    
+    System.out.println(concatenacao);
+   
+    concatenacao = 1 + "1" + 1 + "1";
+    
+    System.out.println(concatenacao);
+   
+    concatenacao = "1" + 1 + 1 + 1;
+    
+    System.out.println(concatenacao);
+    
+    concatenacao = "1" + (1 + 1 + 1);
+   
+    System.out.println(concatenacao);*/
+    
+    
+   int numero = 5;
+
+   System.out.println(numero);
+
+   numero = -numero;
+   
+   System.out.println(numero);
+
+   numero = numero * -1;
+
+   System.out.println(numero);
+
+    }
+
+
+}
